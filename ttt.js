610c46ed2abe2deedfb919e26ea6743c178cc0f3
Bkisembo <bkisembo8@icloud.com>
2025-08-08 12:55:18 +0100

    grid problems

--- ttt.js
+++ ttt.js
@@ -1,16 +1,20 @@
 
 let gridno = 1
 function tttGrid() {
-    const grid = Array.from({ length: 3 }, () => 
-        Array.from({ length: 3 }, () => gridno ++)
-    );
-    
+    grid = []
+    for (let i = 0; i < 3; i++) {
+    grid[i] = []
+    for (let j = 0; j < 3; j++) {
+        grid[i][j] = gridno++;
+    }
+    }
+
     for (let i = 0; i < 3; i++) {
         let rowStr = '|';
         for (let j = 0; j < 3; j++) {
             rowStr += ' ' + grid[i][j] + ' |'
         }
-        console.log(rowStr);
+        console.log(rowStr)
     
         if (i < 3 - 1) {
             console.log('-' + '----'.repeat(3))
